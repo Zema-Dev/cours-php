3a4387d729780c60d3142348f384947e6f91dbbb
Zema-Dev <krobaouattara@gmail.com>
2023-09-16 17:45:35 +0200

    différence operations maths

---
+++ calcul.php
@@ -0,0 +1,11 @@
+<?php
+$somme = 2 + 3;
+$différence = 5 - 6;
+$produit = 4 * 6;
+$quotient = 8 / 2;
+
+echo 'La somme de 2 + 3 = ' . $somme . "\n";
+echo 'La différence de 5 - 6 = ' . $différence . "\n";
+echo 'Le produit de 4 * 6 = ' . $produit . "\n";
+echo 'Le quotient de 8 / 2 = ' . $quotient . "\n";
+?>
