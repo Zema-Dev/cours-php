4d21a9e6914dac579e09087f2e49e744a0afd238
Zema-Dev <krobaouattara@gmail.com>
2023-09-17 15:08:53 +0200

    Ajout d'un constructeur prenant le nom et l'âge à la classe Personne

---
+++ exercice 3/index3.php
@@ -0,0 +1,5 @@
+<?php
+include 'Personne3.php';  // Assurez-vous que le chemin vers Personne.php est correct.
+
+$personne3 = new Personne("Benzema", 30);
+$personne3->afficherDetails();
---
+++ exercice 3/personne3.php
@@ -0,0 +1,15 @@
+<?php
+class Personne {
+    public $nom;
+    public $age;
+
+    public function __construct($nom, $age) {
+        $this->nom = $nom;
+        $this->age = $age;
+    }
+
+    public function afficherDetails() {
+        echo "Nom : " . $this->nom . "\n";
+        echo "Âge : " . $this->age ." ans";
+    }
+}
